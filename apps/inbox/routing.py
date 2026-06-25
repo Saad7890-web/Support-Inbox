623aefa19cce11bd21d306e1e8725d15583691e2
Saad7890-web <omysaadislam@gmail.com>
2026-06-25 23:08:07 +0600

    added websocket routing

---
+++ apps/inbox/routing.py
@@ -0,0 +1,12 @@
+from django.urls import re_path
+
+from apps.inbox.consumers import (
+    ConversationConsumer,
+)
+
+websocket_urlpatterns = [
+    re_path(
+        r"ws/conversations/(?P<conversation_id>\d+)/$",
+        ConversationConsumer.as_asgi(),
+    ),
+]
