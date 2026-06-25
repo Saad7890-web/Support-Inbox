19e57b9a34119c670b84571ea041180525fec602
Saad7890-web <omysaadislam@gmail.com>
2026-06-25 19:32:29 +0600

    create custom user model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+)
 from django.db import models
 
-# Create your models here.
+from apps.accounts.managers import UserManager
+from apps.common.models import TimeStampedModel
+
+
+class User(TimeStampedModel, AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        unique=True,
+        db_index=True,
+    )
+
+    full_name = models.CharField(
+        max_length=255,
+    )
+
+    is_active = models.BooleanField(default=True)
+
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        db_table = "users"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.email
